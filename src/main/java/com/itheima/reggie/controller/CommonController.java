66c3e42f85dc02169f560c30dd3ac400ee1e3d8d
xulily <xuxinlily@outlook.com>
2024-03-19 00:19:37 +0800

    Redis环境的配置

---
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -0,0 +1,94 @@
+package com.itheima.reggie.controller;
+
+import com.alibaba.druid.sql.dialect.sqlserver.ast.SQLServerOutput;
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 负责文件的上传与下载
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+
+    @Value("${reggie.path}")
+    private String basePath;
+
+    /**
+     * 文件上传
+     * @param file
+     * 参数名有要求
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file){
+        log.info(file.toString());
+        //file是一个临时文件 在每次请求完成之后将会被删除
+
+        //获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        //获取文件后缀
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        //使用uuid重命名文件名 防止文件名重复造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix;
+        //创建一个目录对象
+        File dir = new File(basePath);
+        //因为目录不会自动创建 因此手动
+        //判断保存的目录是否存在
+        if(!dir.exists()){//如果该目录不存在 则创建该目录
+            dir.mkdirs();
+        }
+
+        //因此需要将file转存到指定位置
+        try {
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     * @param name
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        try {
+            //输入流 通过输入流读取文件内容
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            //输入流 通过文件输出将文件写回浏览器 在浏览器展示图片
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            //设定返回类型
+            response.setContentType("image/jpeg");
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes)) != -1){
+                outputStream.write(bytes,0,len);//将读取到的内容写入
+                outputStream.flush();//刷新
+            }
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
